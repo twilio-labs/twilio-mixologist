82a3211141209cb05e7fd990b8968db865540ac4
Dominik Kundel <dominik.kundel@gmail.com>
2017-05-02 16:59:40 +0200

    :sparkles: Add Setup and Improve documentation

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,13 @@ const router = express.Router();
 router.use('/orders', require('./orders'));
 router.use('/incoming', require('./incoming'));
 
+router.get('/setup', (req, res, next) => {
+  const webhookurl = req.protocol + '://' + req.get('host') + '/incoming';
+  res.render('setup', { webhookurl });
+});
+
+router.get('*', (req, res, next) => {
+  res.redirect('/orders');
+});
+
 module.exports = router;
