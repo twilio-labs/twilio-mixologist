1c4e07bf69b64f98ab7b5b28ffe492259f30d8a3
Dominik Kundel <dominik.kundel@gmail.com>
2017-07-03 17:53:29 +0200

    :sparkles: Serve front-end with server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api', require('./api'));
 
