9f9284aaf7ec2b48a80cd774d94f02ad3f6cea77
Dominik Kundel <dominik.kundel@gmail.com>
2017-07-03 17:53:18 +0200

    :hammer: Move debug script to webpack

---
+++ preact.config.js
@@ -0,0 +1,13 @@
+/**
+ * Function that mutates original webpack config.
+ * Supports asynchronous changes when promise is returned. 
+ * 
+ * @param {object} config - original webpack config.
+ * @param {object} env - options passed to CLI.
+ * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
+ **/
+export default function(config, env, helpers) {
+  if (!env.ssr) {
+    config.entry['debug'] = './debug.js';
+  }
+}
