4ccc9ea946f419290dbd667ca3563930ca823ebb
Dominik Kundel <dominik.kundel@gmail.com>
2017-07-04 19:47:24 +0200

    :fire: Remove redundant list of default options (moved to config)

--- data/coffee-options.js
+++ data/coffee-options.js
@@ -1,57 +1,6 @@
 const { config } = require('./config');
 
 /**
- * These are all coffee options that can actually be ordered
- */
-const AVAILABLE_OPTIONS = [
-  'Espresso',
-  'Cappuccino',
-  'Latte',
-  'Americano',
-  'Flat White',
-  'Filter Coffee'
-];
-
-/**
- * This is a rudamentary solution to solve typos. All these
- * wrong spellings will map to an actual available coffee.
- */
-const POSSIBLE_OPTIONS = {
-  expreso: 'Espresso',
-  expresso: 'Espresso',
-  espresso: 'Espresso',
-  cappacino: 'Cappuccino',
-  capacino: 'Cappuccino',
-  cappacino: 'Cappuccino',
-  cappocino: 'Cappuccino',
-  capocino: 'Cappuccino',
-  capacino: 'Cappuccino',
-  cappucino: 'Cappuccino',
-  cappuccino: 'Cappuccino',
-  capuccino: 'Cappuccino',
-  capochino: 'Cappuccino',
-  late: 'Latte',
-  lattey: 'Latte',
-  larte: 'Latte',
-  lattee: 'Latte',
-  latte: 'Latte',
-  'cafe late': 'Latte',
-  'caffeé latte': 'Latte',
-  'caffeé latte': 'Latte',
-  'caffe latte': 'Latte',
-  americano: 'Americano',
-  'white americano': 'Americano',
-  caffeé: 'Americano',
-  'flat white': 'Flat White',
-  flatwhite: 'Flat White',
-  'flat-white': 'Flat White',
-  'flatt white': 'Flat White',
-  'filter coffee': 'Filter Coffee',
-  coffee: 'Filter Coffee',
-  'fliter coffee': 'Filter Coffee'
-};
-
-/**
  * Determines based on a message which coffee is trying to be ordered.
  * 
  * @param {any} messageBody 
