1b79076011bfb8a7ed354d2a0fc4ffe51ae76436
Dominik Kundel <dominik.kundel@gmail.com>
2017-07-25 11:37:11 +0200

    :art: Improve structure of preact.config.js

--- preact.config.js
+++ preact.config.js
@@ -7,11 +7,13 @@
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
 export default function(config, env, helpers) {
+  config.node.Buffer = true;
+  config.entry['debug'] = './debug.js';
+
   if (!env.ssr) {
     const plugins = helpers.getPluginsByName(config, 'HtmlWebpackPlugin');
     const htmlPluginOptions = plugins[0].plugin.options;
+
     htmlPluginOptions.excludeChunks.push('debug');
-    config.entry['debug'] = './debug.js';
-    config.node.Buffer = true;
   }
 }
