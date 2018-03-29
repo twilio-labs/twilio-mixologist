0feba0e4a63c9242e31cc23ef8d19481ddef09b8
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-29 10:42:06 +0200

    :sparkles: Load temporarily available options into spelling map

--- server/data/coffee-options.js
+++ server/data/coffee-options.js
@@ -14,14 +14,25 @@ function determineCoffeeFromMessage(messageBody, forEvent) {
   const possibleOptions = config(forEvent).spellingMap;
   const availableOptions = config(forEvent).availableCoffees;
   const message = messageBody.trim().toLowerCase();
-  for (const option of Object.keys(possibleOptions).sort(sortByLengthDesc)) {
-    if (
-      message.indexOf(option) !== -1 &&
-      availableOptions[possibleOptions[option]]
-    ) {
-      return possibleOptions[option];
+
+  // add availableOptions to possibleOptions to avoid missing entries:
+  Object.keys(availableOptions).forEach(name => {
+    possibleOptions[name.toLowerCase()] = name;
+  });
+
+  const optionsSortedByLength = Object.keys(possibleOptions).sort(
+    sortByLengthDesc
+  );
+
+  /* eslint-disable no-restricted-syntax */
+  for (const option of optionsSortedByLength) {
+    const actualPick = possibleOptions[option];
+    if (message.indexOf(option) !== -1 && availableOptions[actualPick]) {
+      return actualPick;
     }
   }
+  /* eslint-enable no-restricted-syntax */
+
   return null;
 }
 
