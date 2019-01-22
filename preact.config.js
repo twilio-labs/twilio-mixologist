e28294e189a5e6e222d3691f3370c74938085b98
Lizzie Siegle <esiegle@brynmawr.edu>
2019-01-22 14:31:41 -0800

    sentry branch remove async

--- preact.config.js
+++ preact.config.js
@@ -19,16 +19,6 @@ export default function(config, env, helpers) {
   });
   config.plugins.push(plugin);
 
-  if (env.isProd) {
-
-    // Make async work
-    let babel = config.module.loaders.filter(loader => loader.loader === 'babel-loader')[0].options;
-    // Blacklist regenerator within env preset:
-    babel.presets[0][1].exclude.push('transform-async-to-generator');
-    // Add fast-async
-    babel.plugins.push([require.resolve('fast-async'), { spec: true }]);
-  }
-
   if (!env.ssr) {
     const plugins = helpers.getPluginsByName(config, 'HtmlWebpackPlugin');
     const htmlPluginOptions = plugins[0].plugin.options;
