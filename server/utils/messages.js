a08c5ba4fb86c381eaff7797da0b2ae73d246877
Marius Obert <obatzer@gmail.com>
2023-07-27 12:15:19 +0200

    format bold

--- server/utils/messages.js
+++ server/utils/messages.js
@@ -25,7 +25,7 @@ const EXISTING_ORDER_MESSAGES = [
 
 // available values: product, orderNumber
 const ORDER_CREATED_MESSAGES = [
-  "Thanks for ordering a ${product} from the Twilio powered Smoothie Bar. Your order number is #${orderNumber}. We'll text you back when it's ready. ${dataPolicy} In the meantime check out this repo ${repoUrl} if you want to see how we built this app. ",
+  "Thanks for ordering a *${product}* from the Twilio powered Smoothie Bar. Your order number is *#${orderNumber}*. We'll text you back when it's ready. ${dataPolicy} In the meantime check out this repo ${repoUrl} if you want to see how we built this app. ",
 ];
 
 // available values: product, orderNumber
