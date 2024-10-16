43fef93089324629301bd8ad6957fe0f906afe22
Marius Obert <obatzer@gmail.com>
2024-10-16 12:56:02 +0200

    render on server instead

--- src/app/(layout-free)/event/[slug]/menu/page.tsx
+++ src/app/(layout-free)/event/[slug]/menu/page.tsx
@@ -1,54 +1,56 @@
-"use client";
-
 import { Event } from "@/app/(master-layout)/event/[slug]/page";
 import MenuItem from "./menu-item";
-import { useSyncMap } from "@/provider/syncProvider";
-import { useEffect, useState } from "react";
 import Header from "./header";
+import { getSyncService } from "@/lib/twilio";
 
-function MenuPage({ params }: { params: { slug: string } }) {
-  const [eventsMap, _, mapInitialized] = useSyncMap(
-    process.env.NEXT_PUBLIC_EVENTS_MAP || "",
-    [params.slug],
-  );
-
-  useEffect(() => {
-    if (mapInitialized) {
-      // @ts-ignore // TODO fix this TS issue
-      const existingEvent = eventsMap?.get(params.slug) as Event;
-      updateInternalEvent(existingEvent);
-    }
-  }, [eventsMap, mapInitialized]);
-
-  const [internalEvent, updateInternalEvent] = useState<Event>();
-
-  if (!internalEvent || !internalEvent.selection) {
-    return <div>Loading...</div>;
+async function MenuPage({ params }: { params: { slug: string } }) {
+  if (!process.env.NEXT_PUBLIC_EVENTS_MAP) {
+    throw new Error("No config doc specified");
   }
 
-  const itemsCount = internalEvent.selection.items.length;
-  const columns = itemsCount % 3 === 0 ? 3 : itemsCount % 4 === 0 ? 4 : 5;
+  const syncService = await getSyncService();
+  try {
+    const events = await syncService
+      .syncMaps()(process.env.NEXT_PUBLIC_EVENTS_MAP)
+      .fetch();
+    const items = await events.syncMapItems().list();
 
-  return (
-    <>
-      <Header number={internalEvent.senders[0]} />
-      <main className="flex grow pb-8 items-center justify-center bg-gray-900 text-white select-none">
-        <div className={`grid grid-cols-${columns} gap-8  `}>
-          {internalEvent.selection?.items.map((item: any, index: Number) => (
-            <div key={`item-${index}`} className="flex flex-col items-center">
-              <div className="p-4 rounded-full mb-2">
-                <MenuItem
-                  title={item.title}
-                  shortTitle={item.shortTitle}
-                  description={item.description}
-                />
+    // @ts-ignore
+    const internalEvent: Event = items.find(
+      (item: any) => item.data.slug === params.slug,
+    ).data;
+
+    if (!internalEvent) {
+      return <div>Event not found</div>;
+    }
+
+    const itemsCount = internalEvent.selection.items.length;
+    const columns = itemsCount % 3 === 0 ? 3 : itemsCount % 4 === 0 ? 4 : 5;
+
+    return (
+      <>
+        <Header number={internalEvent.senders[0]} />
+        <main className="flex grow pb-8 items-center justify-center bg-gray-900 text-white select-none">
+          <div className={`grid grid-cols-${columns} gap-8  `}>
+            {internalEvent.selection?.items.map((item: any, index: Number) => (
+              <div key={`item-${index}`} className="flex flex-col items-center">
+                <div className="p-4 rounded-full mb-2">
+                  <MenuItem
+                    title={item.title}
+                    shortTitle={item.shortTitle}
+                    description={item.description}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </>
-  );
+            ))}
+          </div>
+        </main>
+      </>
+    );
+  } catch (e: any) {
+    console.error(e);
+    throw new Error("Could not fetch events", e);
+  }
 }
 
 export default MenuPage;
