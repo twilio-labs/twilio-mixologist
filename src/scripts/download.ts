087d5e816ce0c519a5bb2f1ceda2bc3566dab670
Marius Obert <obatzer@gmail.com>
2024-10-28 07:05:30 +0100

    Download script to support param to download only one event key

--- src/scripts/download.ts
+++ src/scripts/download.ts
@@ -13,20 +13,30 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
   accountSid: TWILIO_ACCOUNT_SID,
 });
 
+const eventName = process.argv.pop();
+
+if (!eventName || eventName.startsWith("/") || eventName.includes("=")) {
+  console.error(
+    "Please provide an event name as the last argument, e.g. 'pnpm download wearedevs24'",
+  );
+  process.exit(1);
+}
+
 (async () => {
   //fetch all attendees and write to csv file with header columns
   const map = await client.sync.v1
     .services(TWILIO_SYNC_SERVICE_SID)
     .syncMaps("ActiveCustomers");
   try {
-    const mapItems = await map.syncMapItems.list({ limit: 1000 });
+    const mapItems = await map.syncMapItems.list({ limit: 1000 }); //TODO should fetch all here
     const attendees = mapItems
       .map((item) => item.data)
       .filter(
         (a) =>
-          a.stage === Stages.VERIFIED_USER ||
-          a.stage === Stages.FIRST_ORDER ||
-          a.stage === Stages.REPEAT_CUSTOMER,
+          (a.stage === Stages.VERIFIED_USER ||
+            a.stage === Stages.FIRST_ORDER ||
+            a.stage === Stages.REPEAT_CUSTOMER) &&
+          a.event === eventName,
       );
     const csv = attendees.map((attendee) => {
       return `${attendee.email},${attendee.event},${attendee.stage}`;
