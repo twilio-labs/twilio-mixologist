fcda34e45d21f7ee1c978ecb3b57d69443902189
Marius Obert <obatzer@gmail.com>
2024-10-31 15:01:54 +0100

    add kiosk test

---
+++ __tests__/e2e/kiosk-render.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, type Locator } from "@playwright/test";
+import { Privilege } from "@/middleware";
+
+test.describe("[no login]", () => {
+  test("Should not see the page", async ({ page }) => {
+    await page.goto("/event/test-event/kiosk");
+
+    await expect(page.getByText("Unauthorized")).toBeVisible();
+  });
+});
+
+test.describe("[kiosk]", () => {
+  test("Form should be visible", async ({ page, context }) => {
+    await context.addCookies([
+      {
+        name: "privilege",
+        value: Privilege.KIOSK,
+        url: "http://localhost:3000",
+      },
+    ]);
+    context.setExtraHTTPHeaders({
+      Authorization: `Basic ${btoa(process.env.KIOSK_LOGIN || ":")}`,
+    });
+
+    await page.goto("/event/test-event/kiosk");
+
+    await page.getByPlaceholder("Customer name").fill("Test Name");
+    await page.getByLabel("Order Item").click();
+    await page.getByLabel("Espresso", { exact: true }).click();
+    await page
+      .getByPlaceholder("Without regular milk or similar...")
+      .fill("Test Notes");
+    await page
+      .getByRole("button", { name: "Create Order", exact: true })
+      .click();
+    await expect(
+      page.getByRole("button", { name: "Creating...", exact: true }),
+    ).toBeVisible();
+  });
+});
